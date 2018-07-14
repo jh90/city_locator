81b4a8d2bf2c49e4b848ea11f58a6b86677362be
Jake Hamilton <hamiltonjm12@gmail.com>
2018-07-14 12:36:17 -0400

    Scaffold search and result containers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import Search from './search.jsx';
+import Results from './results.jsx';
+
 export default class App extends React.Component {
     constructor () {
         super();
         this.state = {
-
+            city_input: '',
         };
     }
 
+    handleSubmit () {
+
+    }
+
     render () {
-        return ();
+        return (
+            <div>
+                <Search onSubmit={this.handleSubmit} />
+                <Results />
+            </div>
+        );
     }
 }
